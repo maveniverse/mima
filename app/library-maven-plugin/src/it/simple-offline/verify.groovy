b36f2ea8460770a02641b38431494e8aa8e50a21
Tamas Cservenak <tamas@cservenak.net>
2023-03-30 10:18:42 +0200

    Lax IT to make it work with Maven 3.8.x
    As it has different error messages

--- app/library-maven-plugin/src/it/simple-offline/verify.groovy
+++ app/library-maven-plugin/src/it/simple-offline/verify.groovy
@@ -1,5 +1,5 @@
 File buildLog = new File( basedir, 'build.log' )
 assert buildLog.exists()
 assert buildLog.text.contains( "[DEBUG] Runtimes.getRuntime: MavenRuntime" )
-assert buildLog.text.contains( "org.eclipse.aether.resolution.DependencyResolutionException: The following artifacts could not be resolved: junit:junit:jar:4.13.2 (absent)" )
+assert buildLog.text.contains( "junit:junit:jar:4.13.2" ) // minimal: error messages were improved/changed in Maven 3.9.x
 assert buildLog.text.contains( "in offline mode" )
