0f0748bcbc2866fda5965c1096158b8c4ac49237
Tamas Cservenak <tamas@cservenak.net>
2023-04-03 13:27:19 +0200

    Add dummy file to uber module

---
+++ runtime/standalone-sisu-uber/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/uber/Dummy.java
@@ -0,0 +1,6 @@
+package eu.maveniverse.maven.mima.runtime.standalonesisu.uber;
+
+/**
+ * This class is ONLY here to make this module allowed by Central Staging Ruleset (present of sources and javaddoc is must).
+ */
+public class Dummy {}
