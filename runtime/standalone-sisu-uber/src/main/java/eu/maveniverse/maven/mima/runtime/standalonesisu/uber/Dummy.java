96ab3184b007ee06283aceba16765ab0748bbd8e
Tamas Cservenak <tamas@cservenak.net>
2023-04-12 22:02:50 +0200

    Make Dummy dummy

--- runtime/standalone-sisu-uber/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/uber/Dummy.java
+++ runtime/standalone-sisu-uber/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/uber/Dummy.java
@@ -3,4 +3,6 @@ package eu.maveniverse.maven.mima.runtime.standalonesisu.uber;
 /**
  * This class is ONLY here to make this module allowed by Central Staging Ruleset (present of sources and javaddoc is must).
  */
-public class Dummy {}
+public final class Dummy {
+    private Dummy() {}
+}
