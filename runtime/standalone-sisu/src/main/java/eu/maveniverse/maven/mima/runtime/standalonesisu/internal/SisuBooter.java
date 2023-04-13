0053a772a38c86c1a8ae722c3ee7f476da04c2c8
Tamas Cservenak <tamas@cservenak.net>
2023-04-13 16:50:22 +0200

    Fix shutdown
    Do not use shutdownthread, as JVM may NOT go down, but we may.
    Also, to "reboot" in same JVM, we need proper cleanup yet (TBD)

--- runtime/standalone-sisu/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/internal/SisuBooter.java
+++ runtime/standalone-sisu/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/internal/SisuBooter.java
@@ -2,14 +2,17 @@ package eu.maveniverse.maven.mima.runtime.standalonesisu.internal;
 
 import com.google.inject.Guice;
 import com.google.inject.Module;
+import java.io.Closeable;
 import javax.inject.Inject;
 import org.apache.maven.settings.building.SettingsBuilder;
 import org.apache.maven.settings.crypto.SettingsDecrypter;
 import org.eclipse.aether.RepositorySystem;
+import org.eclipse.sisu.bean.LifecycleManager;
+import org.eclipse.sisu.inject.MutableBeanLocator;
 import org.eclipse.sisu.launch.Main;
 import org.eclipse.sisu.space.BeanScanning;
 
-public class SisuBooter {
+public class SisuBooter implements Closeable {
     @Inject
     public RepositorySystem repositorySystem;
 
@@ -19,7 +22,22 @@ public class SisuBooter {
     @Inject
     public SettingsDecrypter settingsDecrypter;
 
-    public static SisuBooter newRepositorySystem() {
+    @Inject
+    public LifecycleManager lifecycleManager;
+
+    @Inject
+    public MutableBeanLocator locator;
+
+    @Override
+    public void close() {
+        try {
+            lifecycleManager.unmanage();
+        } finally {
+            locator.clear();
+        }
+    }
+
+    public static SisuBooter newSisuBooter() {
         final Module app = Main.wire(BeanScanning.CACHE, new SisuModule());
         return Guice.createInjector(app).getInstance(SisuBooter.class);
     }
--- runtime/standalone-sisu/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/internal/SisuModule.java
+++ runtime/standalone-sisu/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/internal/SisuModule.java
@@ -2,28 +2,10 @@ package eu.maveniverse.maven.mima.runtime.standalonesisu.internal;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import javax.inject.Inject;
-import org.eclipse.sisu.inject.MutableBeanLocator;
 
 public class SisuModule implements Module {
     @Override
     public void configure(final Binder binder) {
         binder.bind(SisuBooter.class);
-        binder.bind(ShutdownThread.class).asEagerSingleton();
-    }
-
-    static final class ShutdownThread extends Thread {
-        private final MutableBeanLocator locator;
-
-        @Inject
-        ShutdownThread(final MutableBeanLocator locator) {
-            this.locator = locator;
-            Runtime.getRuntime().addShutdownHook(this);
-        }
-
-        @Override
-        public void run() {
-            locator.clear();
-        }
     }
 }
