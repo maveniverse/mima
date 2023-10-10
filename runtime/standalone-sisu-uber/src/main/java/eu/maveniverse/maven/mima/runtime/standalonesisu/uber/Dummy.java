83dd415d28bc996599cb81945c913ec36996fe69
Tamas Cservenak <tamas@cservenak.net>
2023-10-10 23:30:52 +0200

    Typofix

--- runtime/standalone-sisu-uber/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/uber/Dummy.java
+++ runtime/standalone-sisu-uber/src/main/java/eu/maveniverse/maven/mima/runtime/standalonesisu/uber/Dummy.java
@@ -1,7 +1,7 @@
 package eu.maveniverse.maven.mima.runtime.standalonesisu.uber;
 
 /**
- * This class is ONLY here to make this module allowed by Central Staging Ruleset (present of sources and javaddoc is must).
+ * This class is ONLY here to make this module allowed by Central Staging Ruleset (present of sources and javadoc is must).
  */
 public final class Dummy {
     private Dummy() {}
