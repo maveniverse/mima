b11e86c706a84d94d4511411f667901b3633d186
Tamas Cservenak <tamas@cservenak.net>
2023-10-22 15:48:59 +0200

    WIP

--- cli/src/main/java/eu/maveniverse/maven/mima/cli/Resolve.java
+++ cli/src/main/java/eu/maveniverse/maven/mima/cli/Resolve.java
@@ -1,7 +1,6 @@
 package eu.maveniverse.maven.mima.cli;
 
 import eu.maveniverse.maven.mima.context.Context;
-import java.util.List;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
@@ -11,8 +10,10 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.util.artifact.JavaScopes;
 import org.eclipse.aether.util.filter.DependencyFilterUtils;
+import org.eclipse.aether.util.graph.visitor.PreorderNodeListGenerator;
 import picocli.CommandLine;
 
 /**
@@ -23,6 +24,11 @@ public final class Resolve extends CommandSupport {
 
     @CommandLine.Parameters(index = "0", description = "The GAV to resolve")
     private String gav;
+
+    @CommandLine.Option(
+            names = {"-cp", "--classpath"},
+            description = "Show classpath ready to copy-paste")
+    private boolean classpath;
 
     @Override
     protected Integer doCall(Context context) {
@@ -39,14 +45,20 @@ public final class Resolve extends CommandSupport {
                 new DependencyRequest(collectRequest, DependencyFilterUtils.classpathFilter(JavaScopes.COMPILE));
 
         try {
-            List<ArtifactResult> artifactResults =
-                    system.resolveDependencies(session, dependencyRequest).getArtifactResults();
+            DependencyResult dependencyResult = system.resolveDependencies(session, dependencyRequest);
 
-            for (ArtifactResult artifactResult : artifactResults) {
+            for (ArtifactResult artifactResult : dependencyResult.getArtifactResults()) {
                 logger.info(
                         "{} -> {}",
                         artifactResult.getArtifact(),
                         artifactResult.getArtifact().getFile());
+            }
+
+            if (classpath) {
+                logger.info("");
+                PreorderNodeListGenerator nlg = new PreorderNodeListGenerator();
+                dependencyResult.getRoot().accept(nlg);
+                logger.info("classpath: {}", nlg.getClassPath());
             }
         } catch (DependencyResolutionException e) {
             throw new RuntimeException(e);
