4edceb3482a7b2b6a2a34405b3acc511eda04667
Tamas Cservenak <tamas@cservenak.net>
2023-12-09 18:03:08 +0100

    Now that MR is fixed, return apache transport in play
    So, now:
    java8 uses apache
    java11+ uses jdk (takes over)

--- runtime/standalone-static/src/main/java/eu/maveniverse/maven/mima/runtime/standalonestatic/RepositorySystemSupplier.java
+++ runtime/standalone-static/src/main/java/eu/maveniverse/maven/mima/runtime/standalonestatic/RepositorySystemSupplier.java
@@ -1,9 +1,7 @@
 package eu.maveniverse.maven.mima.runtime.standalonestatic;
 
-import java.util.HashMap;
 import java.util.Map;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
-import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.jdk.JdkTransporterFactory;
 
 /**
@@ -12,8 +10,7 @@ import org.eclipse.aether.transport.jdk.JdkTransporterFactory;
 public class RepositorySystemSupplier extends org.eclipse.aether.supplier.RepositorySystemSupplier {
     @Override
     protected Map<String, TransporterFactory> getTransporterFactories() {
-        HashMap<String, TransporterFactory> result = new HashMap<>();
-        result.put(FileTransporterFactory.NAME, new FileTransporterFactory());
+        Map<String, TransporterFactory> result = super.getTransporterFactories();
         result.put(JdkTransporterFactory.NAME, new JdkTransporterFactory());
         return result;
     }
