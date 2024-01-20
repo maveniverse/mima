ee44fcec4568ea890d38be6851406bf5bbf4ff39
Tamas Cservenak <tamas@cservenak.net>
2024-01-20 13:10:33 +0100

    Identify should accept sha1 or file
    And if file, SHA1-it and use that. Also sort out
    some verbosity.

--- cli/src/main/java/eu/maveniverse/maven/mima/cli/Identify.java
+++ cli/src/main/java/eu/maveniverse/maven/mima/cli/Identify.java
@@ -2,11 +2,17 @@ package eu.maveniverse.maven.mima.cli;
 
 import static org.apache.maven.search.api.request.FieldQuery.fieldQuery;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import org.apache.maven.search.api.MAVEN;
 import org.apache.maven.search.api.SearchBackend;
 import org.apache.maven.search.api.SearchRequest;
 import org.apache.maven.search.api.SearchResponse;
+import org.eclipse.aether.util.ChecksumUtils;
 import picocli.CommandLine;
 
 /**
@@ -15,11 +21,33 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "identify", description = "Identifies Maven Artifacts")
 public final class Identify extends SearchCommandSupport {
 
-    @CommandLine.Parameters(index = "0", description = "The sha1 checksum to identify artifact with")
-    private String sha1;
+    @CommandLine.Parameters(index = "0", description = "File or sha1 checksum to identify artifact with")
+    private String target;
 
     @Override
     protected Integer doCall() throws IOException {
+        String sha1;
+        if (Files.exists(Paths.get(target))) {
+            try {
+                MessageDigest sha1md = MessageDigest.getInstance("SHA-1");
+
+                byte[] buf = new byte[8192];
+                int read;
+                try (FileInputStream fis = new FileInputStream(target)) {
+                    read = fis.read(buf);
+                    while (read != -1) {
+                        sha1md.update(buf, 0, read);
+                        read = fis.read(buf);
+                    }
+                }
+                sha1 = ChecksumUtils.toHexString(sha1md.digest());
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException("SHA1 MessageDigest unavailable", e);
+            }
+        } else {
+            sha1 = target;
+        }
+        verbose("Identifying artifact with SHA1={}", sha1);
         try (SearchBackend backend = getSmoBackend(repositoryId)) {
             SearchRequest searchRequest = new SearchRequest(fieldQuery(MAVEN.SHA1, sha1));
             SearchResponse searchResponse = backend.search(searchRequest);
