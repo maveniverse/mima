57239d4988cc8b60e85460fef653a0dafe019f59
Tamas Cservenak <tamas@cservenak.net>
2024-02-02 14:47:22 +0100

    Comment out new classes

--- demo/library/src/main/java/eu/maveniverse/maven/mima/impl/library/Classpath.java
+++ demo/library/src/main/java/eu/maveniverse/maven/mima/impl/library/Classpath.java
@@ -23,8 +23,6 @@ import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.repository.RepositoryPolicy;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
-import org.eclipse.aether.spi.connector.checksum.ChecksumAlgorithmFactory;
-import org.eclipse.aether.spi.connector.checksum.ChecksumAlgorithmHelper;
 import org.eclipse.aether.util.graph.visitor.PreorderNodeListGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -92,15 +90,16 @@ public class Classpath {
         PreorderNodeListGenerator nlg = new PreorderNodeListGenerator();
         rootNode.accept(nlg);
 
-        ChecksumAlgorithmFactory sha1 = context.lookup().lookup(ChecksumAlgorithmFactory.class, "SHA-1").orElseThrow(() -> new IllegalStateException("component not found"));
-        for (File file : nlg.getFiles()) {
-            try {
-                Map<String, String> hashes = ChecksumAlgorithmHelper.calculate(file, Collections.singletonList(sha1));
-                logger.info("{}({})={}", sha1.getName(), file.getName(), hashes.get(sha1.getName()));
-            } catch (IOException e) {
-                logger.warn("IO problem while calculating sha1 of {}", file, e);
-            }
-        }
+        // This works only in Maven 3.9.x + Resolver 1.9.x (these classes were introduced in recent resolver)
+//        ChecksumAlgorithmFactory sha1 = context.lookup().lookup(ChecksumAlgorithmFactory.class, "SHA-1").orElseThrow(() -> new IllegalStateException("component not found"));
+//        for (File file : nlg.getFiles()) {
+//            try {
+//                Map<String, String> hashes = ChecksumAlgorithmHelper.calculate(file, Collections.singletonList(sha1));
+//                logger.info("{}({})={}", sha1.getName(), file.getName(), hashes.get(sha1.getName()));
+//            } catch (IOException e) {
+//                logger.warn("IO problem while calculating sha1 of {}", file, e);
+//            }
+//        }
         return nlg.getClassPath();
     }
 
