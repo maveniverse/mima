cc8dd3ba910083e93d61859edbe649939f24bd17
Tamas Cservenak <tamas@cservenak.net>
2024-03-08 15:01:57 +0100

    Reformat

--- cli/src/main/java/eu/maveniverse/maven/mima/cli/SearchCommandSupport.java
+++ cli/src/main/java/eu/maveniverse/maven/mima/cli/SearchCommandSupport.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
 package eu.maveniverse.maven.mima.cli;
 
 import static org.apache.maven.search.api.request.BooleanQuery.and;
