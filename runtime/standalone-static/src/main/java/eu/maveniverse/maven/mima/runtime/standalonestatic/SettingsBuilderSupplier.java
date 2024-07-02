3db3a72adf69b6327fb608d651eded43efed9fca
Tamas Cservenak <tamas@cservenak.net>
2024-07-02 15:37:13 +0200

    Go for Maven4 still.

--- runtime/standalone-static/src/main/java/eu/maveniverse/maven/mima/runtime/standalonestatic/SettingsBuilderSupplier.java
+++ runtime/standalone-static/src/main/java/eu/maveniverse/maven/mima/runtime/standalonestatic/SettingsBuilderSupplier.java
@@ -8,11 +8,9 @@
 package eu.maveniverse.maven.mima.runtime.standalonestatic;
 
 import java.util.function.Supplier;
+import org.apache.maven.internal.impl.DefaultSettingsXmlFactory;
 import org.apache.maven.settings.building.DefaultSettingsBuilder;
 import org.apache.maven.settings.building.SettingsBuilder;
-import org.apache.maven.settings.io.DefaultSettingsReader;
-import org.apache.maven.settings.io.DefaultSettingsWriter;
-import org.apache.maven.settings.validation.DefaultSettingsValidator;
 
 /**
  * Override to customize.
@@ -21,6 +19,6 @@ public class SettingsBuilderSupplier implements Supplier<SettingsBuilder> {
     @Override
     public SettingsBuilder get() {
         return new DefaultSettingsBuilder(
-                new DefaultSettingsReader(), new DefaultSettingsWriter(), new DefaultSettingsValidator());
+                new org.apache.maven.internal.impl.DefaultSettingsBuilder(), new DefaultSettingsXmlFactory());
     }
 }
