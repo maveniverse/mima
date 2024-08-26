02f02328541f20321978fadd8aa0d4b8dae7b982
Tamas Cservenak <tamas@cservenak.net>
2024-08-26 21:12:36 +0200

    feat: MMR extension for MIMA

---
+++ extensions/mmr/src/main/java/eu/maveniverse/maven/mima/extensions/mmr/MavenModelResolver.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mima.extensions.mmr;
+
+import static java.util.Objects.requireNonNull;
+
+import eu.maveniverse.maven.mima.context.Context;
+import eu.maveniverse.maven.mima.extensions.mmr.internal.ArtifactDescriptorReaderImpl;
+import org.apache.maven.model.building.ModelBuilder;
+import org.apache.maven.repository.internal.DefaultModelCacheFactory;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.impl.RemoteRepositoryManager;
+import org.eclipse.aether.impl.RepositoryEventDispatcher;
+import org.eclipse.aether.resolution.ArtifactDescriptorException;
+import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
+import org.eclipse.aether.resolution.ArtifactDescriptorResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MavenModelResolver {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final ArtifactDescriptorReaderImpl artifactDescriptorReader;
+    private final RepositorySystemSession session;
+
+    public MavenModelResolver(Context context) {
+        requireNonNull(context, "context");
+        this.artifactDescriptorReader = new ArtifactDescriptorReaderImpl(
+                context.repositorySystem(),
+                context.lookup()
+                        .lookup(RemoteRepositoryManager.class)
+                        .orElseThrow(() -> new IllegalStateException("RemoteRepositoryManager not available")),
+                context.lookup()
+                        .lookup(ModelBuilder.class)
+                        .orElseThrow(() -> new IllegalStateException("Model builder not available")),
+                context.lookup()
+                        .lookup(RepositoryEventDispatcher.class)
+                        .orElseThrow(() -> new IllegalStateException("EventDispatcher not available")),
+                new DefaultModelCacheFactory());
+        this.session = requireNonNull(context.repositorySystemSession(), "session");
+    }
+
+    public ArtifactDescriptorResult readEffectiveModel(ArtifactDescriptorRequest request)
+            throws ArtifactDescriptorException {
+        return artifactDescriptorReader.readEffectiveArtifactDescriptor(session, request);
+    }
+
+    public ArtifactDescriptorResult readRawModel(ArtifactDescriptorRequest request) throws ArtifactDescriptorException {
+        return artifactDescriptorReader.readRawArtifactDescriptor(session, request);
+    }
+}
