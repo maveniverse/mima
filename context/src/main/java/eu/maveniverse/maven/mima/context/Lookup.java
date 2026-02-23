bcab11b29dec3eeea36344e7423ac1a67382a2b5
Tamas Cservenak <tamas@cservenak.net>
2026-02-23 12:54:09 +0100

    License header updates (#247)
    Parent 50 carried some updates, apply them.

--- context/src/main/java/eu/maveniverse/maven/mima/context/Lookup.java
+++ context/src/main/java/eu/maveniverse/maven/mima/context/Lookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Maveniverse Org.
+ * Copyright (c) 2023-2026 Maveniverse Org.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
